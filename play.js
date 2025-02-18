557c7d19d41dc83c70f375ec5511f92fbf401f74
John Robideaux <jrobideaux@fugro.com>
2025-02-18 10:04:19 -0600

    Randomizer and better checking

--- play.js
+++ play.js
@@ -35,10 +35,45 @@ checkButton.onclick = () => {
   checkGame()
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function loadGame() {
-  currentPhrases = currentGame.phrases
+  currentPhrases = [...currentGame.phrases]
+
   srcPanel = byId('drag-src')
   srcPanel.textContent = null
+
+  // add phrases in random order
+  numPhrases = currentPhrases.length 
+  while (numPhrases > 0) {
+    let index = getRandomIntInclusive(0, numPhrases-1);
+    node = currentPhrases[index]
+    currentPhrases.splice(index, 1)
+
+    phrase = document.createElement('div')
+    phrase.classList.add("phrase")
+    phrase.setAttribute('title', node.phrase)
+    if (node.short) {
+      phrase.setAttribute('title', node.short)
+    }
+    div = document.createElement('div')
+    text = document.createTextNode(node.phrase)
+    div.appendChild(text)
+    // div.setAttribute('id', "phrase_" + index)
+    node.groupIds.forEach(id => {
+      div.classList.add("G" + id)
+    })
+    phrase.appendChild(div)
+
+    srcPanel.appendChild(phrase)
+    numPhrases = currentPhrases.length 
+  }
+
+  /*
   currentPhrases.forEach((element, index) => {
     phrase = document.createElement('div')
     phrase.classList.add("phrase")
@@ -57,6 +92,7 @@ function loadGame() {
 
     srcPanel.appendChild(phrase)
   });
+*/
 
   slots = []
   slots.push(srcPanel)
@@ -87,6 +123,22 @@ function checkGame() {
   circles = ["red", "blue", "green"]
   groups.forEach((group, index) => {
     phrases = byClass(group)
+
+    for (let ci=0; ci<circles.length; i++) {
+      circle = circles[ci]
+      label = byId(circle+"label")
+      if (arePhrasesInCircle(phrases, circle))
+      {
+        circles.splice(ci, 1)
+        console.log(circle + " Circle is solved!")
+        label.textContent = '"' + currentGame.groups[index].label + '"'
+        break
+      } else {
+        label.textContent = circle + ' group'
+      }
+    }
+
+    /*
     circles.forEach((circle, ci) => {
       label = byId(circle+"label")
       if (arePhrasesInCircle(phrases, circle))
@@ -98,10 +150,15 @@ function checkGame() {
         label.textContent = circle + ' group'
       }
     })
+    */
   })
 }
 
 function arePhrasesInCircle(phrases, circle) {
+  if (!phrases || phrases.length < 1) {
+    return false
+  }
+  
   found = true
   for (let phrase of phrases) {
     parent = phrase.parentElement
