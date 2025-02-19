f487616cb651dbda51fa3b350376347fd712685c
John Robideaux <jrobideaux@fugro.com>
2025-02-19 15:37:20 -0600

    Added jdr01 game

---
+++ games/jdr01.js
@@ -0,0 +1,56 @@
+game = {
+    title: "JDR Game 01",
+    groups: [
+        {
+            id: 1,
+            label: "Do Re Me Fa So La Ti Do"
+        },
+        {
+            id: 2,
+            label: "Actual Words"
+        },
+        {
+            id: 3,
+            label: "State Abbreviations"
+        }
+    ],
+    phrases: [
+        {
+            phrase: "FA",
+            short: "",
+            groupIds: [1]
+        },
+        {
+            phrase: "IF",
+            short: "",
+            groupIds: [2]
+        },
+        {
+            phrase: "NE",
+            short: "",
+            groupIds: [3]
+        },
+        {
+            phrase: "DO",
+            short: "",
+            groupIds: [1,2]
+        },
+        {
+            phrase: "OR",
+            short: "",
+            groupIds: [2,3]
+        },
+        {
+            phrase: "LA",
+            short: "",
+            groupIds: [1,3]
+        },
+        {
+            phrase: "ME",
+            short: "",
+            groupIds: [1,2,3]
+        }
+    ]
+}
+
+games.push(game)
