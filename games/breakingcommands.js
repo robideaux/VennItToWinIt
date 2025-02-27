67a13d577710444e495d596c6a098af4c3deb6f0
Donald Schoolmaster <donaldschoolmaster@Skippy.local>
2025-02-27 15:21:58 -0600

    games to json

---
+++ games/breakingcommands.js
@@ -0,0 +1,33 @@
+game = {
+   title: "Breaking Commands",
+   groups: [
+       "Directives",
+       "Road Signs",
+       "Clogged"
+      ],
+      phrases: {
+        "Move": {
+          groupIds: [1]
+        },
+        "Dip": {
+          groupIds: [2]
+        },
+        "Dam": {
+        groupIds: [3]
+      },
+      "Yield": {
+        groupIds: [1,2]
+      },
+       "No Outlet": {
+         groupIds: [2,3]
+       },
+       "Catch": {
+         groupIds: [1,3]
+       },
+       "Stop": {
+         groupIds: [1,2,3]
+       }
+     }
+   }
+
+games.push(game)
